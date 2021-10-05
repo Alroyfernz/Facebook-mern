ebabd6bde16fa787b05b212d0dce0504c2379c7f
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-10-05 19:47:32 +0530

    fixed the logout bug

--- client/src/components/Homeheader.js
+++ client/src/components/Homeheader.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./homeheader.css";
 import axios from "axios";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { AiFillCaretDown } from "react-icons/ai";
 import { BsArrowBarLeft, BsArrowBarRight, BsGrid3X3Gap } from "react-icons/bs";
 import { BsSearch } from "react-icons/bs";
@@ -17,6 +17,7 @@ const Homeheader = ({ selected }) => {
   const [rightO, setRightO] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
   const [filteredUsers, setFilteredUsers] = useState([]);
+  const { userInfo } = useSelector((state) => state.userLogin);
   const logout = () => {
     dispatch({ type: USER_LOGOUT });
     localStorage.removeItem("userInfo");
@@ -163,9 +164,9 @@ const Homeheader = ({ selected }) => {
         // className="homeHeader__otherIcons"
       >
         <div class="round profile">
-          <a href="#">
+          <a href="#" style={{ textDecoration: "none" }}>
             <Avatar className="ProfileAvatar" src="" />
-            <p>alroy</p>
+            <p>{userInfo.name}</p>
           </a>
         </div>
         <div
@@ -202,7 +203,7 @@ const Homeheader = ({ selected }) => {
           </svg>
         </div>
 
-        <div
+        {/* <div
           class="round"
           style={{
             display: "flex",
@@ -215,13 +216,16 @@ const Homeheader = ({ selected }) => {
           <svg
             viewBox="0 0 28 28"
             alt=""
-            // class={`notificationsIcon ${notificationsOpen && "blue"}`}
+            className="setIconColor"
             height="20"
             width="20"
           >
-            <path d="M7.847 23.488C9.207 23.488 11.443 23.363 14.467 22.806 13.944 24.228 12.581 25.247 10.98 25.247 9.649 25.247 8.483 24.542 7.825 23.488L7.847 23.488ZM24.923 15.73C25.17 17.002 24.278 18.127 22.27 19.076 21.17 19.595 18.724 20.583 14.684 21.369 11.568 21.974 9.285 22.113 7.848 22.113 7.421 22.113 7.068 22.101 6.79 22.085 4.574 21.958 3.324 21.248 3.077 19.976 2.702 18.049 3.295 17.305 4.278 16.073L4.537 15.748C5.2 14.907 5.459 14.081 5.035 11.902 4.086 7.022 6.284 3.687 11.064 2.753 15.846 1.83 19.134 4.096 20.083 8.977 20.506 11.156 21.056 11.824 21.986 12.355L21.986 12.356 22.348 12.561C23.72 13.335 24.548 13.802 24.923 15.73Z"></path>
+            <path
+              className="setIconColor"
+              d="M7.847 23.488C9.207 23.488 11.443 23.363 14.467 22.806 13.944 24.228 12.581 25.247 10.98 25.247 9.649 25.247 8.483 24.542 7.825 23.488L7.847 23.488ZM24.923 15.73C25.17 17.002 24.278 18.127 22.27 19.076 21.17 19.595 18.724 20.583 14.684 21.369 11.568 21.974 9.285 22.113 7.848 22.113 7.421 22.113 7.068 22.101 6.79 22.085 4.574 21.958 3.324 21.248 3.077 19.976 2.702 18.049 3.295 17.305 4.278 16.073L4.537 15.748C5.2 14.907 5.459 14.081 5.035 11.902 4.086 7.022 6.284 3.687 11.064 2.753 15.846 1.83 19.134 4.096 20.083 8.977 20.506 11.156 21.056 11.824 21.986 12.355L21.986 12.356 22.348 12.561C23.72 13.335 24.548 13.802 24.923 15.73Z"
+            ></path>
           </svg>
-        </div>
+        </div> */}
         {/* <div class="dropdown-content2">
           <h1>Notifications</h1>
 
@@ -260,7 +264,10 @@ const Homeheader = ({ selected }) => {
             height: "40px",
           }}
         >
-          <AiFillCaretDown onClick={() => setRightO(!rightO)} />
+          <AiFillCaretDown
+            className="setIconColor"
+            onClick={() => setRightO(!rightO)}
+          />
           {/* <i class={`dropdownIcon ${profileDown === true && "blue"}`} /> */}
           <div
             className={
@@ -275,7 +282,7 @@ const Homeheader = ({ selected }) => {
                   class="Avatar"
                 />
                 <div class="sideinfoDropAvatar">
-                  <h1>{user?.displayName}</h1>
+                  <h1>{userInfo.name}</h1>
                   <p>See your profile</p>
                 </div>
               </div>
