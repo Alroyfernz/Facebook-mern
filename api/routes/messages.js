22cb39c498baeba4261aad4c6cc385263076e631
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-10-08 21:15:49 +0530

    done with building rest api for messages

--- api/routes/messages.js
+++ api/routes/messages.js
@@ -1,3 +1,25 @@
 const router = require("express").Router();
 const Message = require("../models/Message");
+
+router.post("/", async (req, res) => {
+  const newMessage = new Message(req.body);
+  try {
+    const savedMessage = await newMessage.save();
+    res.status(200).json(savedMessage);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+router.get("/conversationId", async (req, res) => {
+  try {
+    const allMessages = await Message.find({
+      conversationId: req.params.conversationId,
+    });
+    res.status(200).json(allMessages);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
 module.exports = router;
