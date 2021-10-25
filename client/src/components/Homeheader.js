46ef86f78e71971918e22ca741bf571b46557b7b
Alroy Fernandes <alroyfernandes07518@gmail.com>
2021-10-26 01:14:27 +0530

    fixed some bugs in homeheader

--- client/src/components/Homeheader.js
+++ client/src/components/Homeheader.js
@@ -17,7 +17,7 @@ const Homeheader = ({ selected }) => {
   const dispatch = useDispatch();
   const [rightO, setRightO] = useState(false);
   const [searchTerm, setSearchTerm] = useState("");
-  const [filteredUsers, setFilteredUsers] = useState([]);
+
   const { userInfo } = useSelector((state) => state.userLogin);
   const logout = () => {
     dispatch({ type: USER_LOGOUT });
@@ -74,17 +74,6 @@ const Homeheader = ({ selected }) => {
     fetchUser();
   }, []);
 
-  // const updateSearchResult = (e) => {
-  //   setSearchTerm(e.target.value);
-  //   document.getElementsByClassName("dropdown-content3")[0].style.display =
-  //     "block";
-  //   // filteredUsers = users.filter((user) => user !== null);
-  //   setFilteredUsers(users.filter((user) => user.name === searchTerm));
-  //   console.log(searchTerm);
-
-  // console.log(user, "from main page log bro");
-  // console.log(userInfo);
-
   return (
     <div className="homeHeader">
       <div className="homeHeaderLogoAndSearch">
@@ -105,7 +94,6 @@ const Homeheader = ({ selected }) => {
           type="text"
           placeholder="Search Facebook"
           className="searchBox"
-          // onChange={updateSearchResult}
           onChange={(e) => {
             setSearchTerm(e.target.value);
             document.getElementsByClassName(
@@ -198,7 +186,6 @@ const Homeheader = ({ selected }) => {
             ? "homeHeader__otherIcons open"
             : "homeHeader__otherIcons"
         }
-        // className="homeHeader__otherIcons"
       >
         <div
           class="round profile"
@@ -207,11 +194,6 @@ const Homeheader = ({ selected }) => {
           }}
         >
           <a href="#" style={{ textDecoration: "none" }}>
-            {/* <Avatar
-              src="https://res.cloudinary.com/dsolobwu0/image/upload/v1633544297/facebookImages/xgs2tjsqy33hb5ube65p.png"
-              className="ProfileAvatar"
-              src=""
-            /> */}
             <img
               src={
                 userInfo?.profilePicture !== null
@@ -261,56 +243,6 @@ const Homeheader = ({ selected }) => {
           </svg>
         </div>
 
-        {/* <div
-          class="round"
-          style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "center",
-            width: "56px",
-            height: "40px",
-          }}
-        >
-          <svg
-            viewBox="0 0 28 28"
-            alt=""
-            className="setIconColor"
-            height="20"
-            width="20"
-          >
-            <path
-              className="setIconColor"
-              d="M7.847 23.488C9.207 23.488 11.443 23.363 14.467 22.806 13.944 24.228 12.581 25.247 10.98 25.247 9.649 25.247 8.483 24.542 7.825 23.488L7.847 23.488ZM24.923 15.73C25.17 17.002 24.278 18.127 22.27 19.076 21.17 19.595 18.724 20.583 14.684 21.369 11.568 21.974 9.285 22.113 7.848 22.113 7.421 22.113 7.068 22.101 6.79 22.085 4.574 21.958 3.324 21.248 3.077 19.976 2.702 18.049 3.295 17.305 4.278 16.073L4.537 15.748C5.2 14.907 5.459 14.081 5.035 11.902 4.086 7.022 6.284 3.687 11.064 2.753 15.846 1.83 19.134 4.096 20.083 8.977 20.506 11.156 21.056 11.824 21.986 12.355L21.986 12.356 22.348 12.561C23.72 13.335 24.548 13.802 24.923 15.73Z"
-            ></path>
-          </svg>
-        </div> */}
-        {/* <div class="dropdown-content2">
-          <h1>Notifications</h1>
-
-          {
-            notifications.length == 0 ? (
-              <div className="noNotifDiv">
-                <img src={user.photoURL} />
-                <h1 className="NoNotif">It seems that there are no active notifications</h1>
-              </div>
-            ) : (
-                console.log()
-              )
-          }
-          {
-            notifications.map(({ notification }) => (
-              <a href="#" className="announcement">
-                <div class="optionDrop">
-                  <Avatar src="" />
-                  <div className="announcementInfo">
-                    <h1>{notification.username} <span>commented to your post.</span></h1>
-                  </div>
-                </div>
-              </a>
-            ))
-          }
-
-        </div> */}
         <div
           class="round"
           onClick={renderProfile}
@@ -326,7 +258,7 @@ const Homeheader = ({ selected }) => {
             className="setIconColor"
             onClick={() => setRightO(!rightO)}
           />
-          {/* <i class={`dropdownIcon ${profileDown === true && "blue"}`} /> */}
+
           <div
             className={
               rightO == true ? "dropdown-content block" : "dropdown-content"
